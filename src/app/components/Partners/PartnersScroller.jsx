b089f3cd85ae60696f498bbf38fcdc558ecd538b
Daniel Meduoye <meduoyedaniel6@gmail.com>
2025-11-04 11:58:57 +0100

    Fixed bug

--- src/app/components/Partners/PartnersScroller.jsx
+++ src/app/components/Partners/PartnersScroller.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import styles from "./partnersScroller.module.css";
+import styles from "./PartnersScroller.module.css";
 import Image from "next/image";
 
 export default function PartnersScroller() {
